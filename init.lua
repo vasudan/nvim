81d940ea7057ce985eefde5ec4cbb96bb5fa0ede
Adam <noreply@johnlabod.com>
2026-06-26 17:26:27 -0400

    fix on windows

--- init.lua
+++ init.lua
@@ -18,11 +18,18 @@ vim.opt.rtp:prepend(lazypath)
 local sysName = vim.loop.os_uname().sysname
 local isWin = sysName:find 'Windows' and true or false
 if isWin then
-    if vim.fn.executable("pwsh") == 1 then
-        vim.opt.shell = "pwsh" --"pwsh" for 7.x if installed
-    else
-        vim.opt.shell = "powershell" --"powershell" for 5.x
-    end
+  -- All of these options are required to make powershell work
+  vim.opt.shellxquote = ''
+  vim.opt.shellquote = ''
+  vim.opt.shellcmdflag = '-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command '
+	vim.opt.shellpipe = '| Out-File -Encoding UTF8 %s'
+	vim.opt.shellredir = '| Out-File -Encoding UTF8 %s'
+  if vim.fn.executable("pwsh") == 1 then
+    vim.opt.shell = "pwsh" --"pwsh" for 7.x if installed
+    vim.opt.shellcmdflag = vim.opt.shellcmdflag + " $PSStyle.OutputRendering = 'PlainText';"
+  else
+    vim.opt.shell = "powershell" --"powershell" for 5.x
+  end
 end
 
 -- validate that lazy is available
